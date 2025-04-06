59627ace0fb68a5e9bf71982a3c8b6941a831c77
maikssilva <maikstrader@gmail.com>
2025-04-06 19:54:44 -0300

    fundamentos de go

---
+++ dio-go/inferencia/aula5.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nome string = "Maik"
+	var idade int = 32
+	var versao float32 = 2.2		
+	fmt.Println("Olá sr.", nome, "sua idade é", idade)
+	fmt.Println("Este programa está na versão:", versao)
+	
+}
